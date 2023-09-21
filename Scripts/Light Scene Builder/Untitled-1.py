9e987064934981f9a4f782b469a0b975abd0c871
Darren Bui <thanhdat.d.bui-1@student.uts.edu.au>
2023-09-21 21:21:48 +1000

    poopy pseudo code

--- Scripts/Light Scene Builder/Untitled-1.py
+++ Scripts/Light Scene Builder/Untitled-1.py
@@ -1,1 +1,26 @@
 print("placeholder")
+
+importFiles()
+pathOfFiles = (yadda yadda file name blah blah) //specifics file path
+fileType = (file types OBJ, Image, SVG, etc) //initialises function for specific file type
+
+files = getFileList(folder/directory path) //grabs file list
+	if files == 0; // else if statement for finding file
+	print("No Files Found)
+	else
+		file(pathOfFiles, i = true) 
+
+versionCheck()
+	load SQLite database
+	
+	corroborate database
+
+	if asset version not matched with one on local system
+		download and replace old asset w/ new
+	else return asset is already latest version
+
+lightGlobal()
+
+lightSpot()
+
+lightGround()
