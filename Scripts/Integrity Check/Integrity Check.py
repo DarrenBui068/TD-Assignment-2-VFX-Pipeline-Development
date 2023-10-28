ea1daff28e7e15bbc4107e9c8514e4d31ac17d2f
dadysherwin1 <86547852+dadysherwin1@users.noreply.github.com>
2023-10-28 22:01:56 +1100

    Added check selection window UI
    Added labelled checkboxes to the check selection window

--- Scripts/Integrity Check/Integrity Check.py
+++ Scripts/Integrity Check/Integrity Check.py
@@ -1,129 +1,48 @@
-Deletes unknown/unused nodes 
+import maya.cmds as cmds
 
-FUNCTION deleteBlankNodes()
-	FOR each node in scene
-		IF node is blank
-			delete node
-		END IF
-	END FOR
-END FUNCTION
-	
+if cmds.window('IntegrityCheck', exists = True):
+    cmds.deleteUI('IntegrityCheck')
+window1 = cmds.window('IntegrityCheck', resizeToFitChildren=True)
+cmds.columnLayout(columnAttach=("left", 10))
+cmds.separator(h=10,style='none')
 
-Checks naming convention of the asset
+cmds.text("Select properties to check")
+cmds.separator(h=10,style='none')
 
-FUNCTION checkName(object)
-IF object name good
-RETURN true
-   	ELSE
-		print “Object name is invalid”
-        		RETURN false
-   	END IF
-END FUNCTION
+# General
+cmds.checkBox("General", annotation="Selects/deselects all General checks")
+cmds.columnLayout(columnAttach=("left", 35))
+cmds.checkBox("Unused nodes", annotation="Check if theres any empty objects")
+cmds.checkBox("Asset naming", annotation="Checks object names with naming convention")
+cmds.checkBox("Node hierarchy", annotation="Checks if the layout of the scene is readable for publishing")
+cmds.checkBox("Reference errors", annotation="Checks for invalid references")
+cmds.checkBox("Very small decimals", annotation="Checks for abnormally long (5+) decimal points")
+cmds.separator(h=10,style='none')
 
+# Layout
+cmds.setParent("..")
+cmds.checkBox("Layout", annotation="Selects/deselects all Layout checks")
+cmds.columnLayout(columnAttach=("left", 35))
+cmds.checkBox("Camera apeture", annotation="Checks if the camera apeture is 16:9 aspect ratio")
+cmds.checkBox("Focal length", annotation="Checks if the camera focal length is an appropriate value (12, 14, 16, 18, 21, 25, 27, 32, 35, 40, 50, 65, 75, 100, 135, 150)")
+cmds.checkBox("F-Stop", annotation="Checks if the camera f-stop is an appropriate value (1.3, 2, 2.8, 4, 5.6, 8, 11, 16, 22)")
+cmds.separator(h=10,style='none')
 
-Round very small decimals
+# Set pieces
+cmds.setParent("..")
+cmds.checkBox("Set Piece", annotation="Selects/deselects all Set Piece checks")
+cmds.columnLayout(columnAttach=("left", 35))
+cmds.checkBox("Transform", annotation="Checks if the transform is at the origin")
+cmds.checkBox("Pivot", annotation="Checks if the pivot is at the origin")
+cmds.separator(h=10,style='none')
 
-FUNCTION roundAttributes(object)
-   	FOR each attribute in object
-        		IF attribute is floating point and has 5 dp
-            		round to 4 dp
-        		END IF
-    	END FOR
-END FUNCTION
+# Sets
+cmds.setParent("..")
+cmds.checkBox("Set", annotation="Selects/deselects all Set checks")
+cmds.columnLayout(columnAttach=("left", 35))
+cmds.checkBox("Transform", annotation="Checks if the transform is at the origin")
+cmds.checkBox("Pivot", annotation="Checks if the pivot is at the origin")
+cmds.checkBox("Asset version", annotation="Checks if each referenced asset is the latest version")
+cmds.separator(h=10,style='none')
 
-
-Set position and pivot to origin
-
-FUNCTION centerObject(object)
-	set position to 0,0,0
-	set pivot to 0,0,0
-END FUNCTION
-
-
-
-
-Check for invalid set references
-
-FUNCTION checkReferences(set)
-	FOR each setPiece
-		IF invalid reference
-			print “Reference error in “ [setPiece]
-	RETURN true
-END FUNCTION
-
-
-Validate asset with the above functions
-
-FUNCTION asset()
-get asset from scene
-    	IF checkName(asset)
-        		print "This asset is ok"
-    	END IF
-
-	centerObject(asset)
-	roundAttributes(asset)
-	deleteBlankNodes()
-END FUNCTION
-
-
-Validate set with the above functions
-
-FUNCTION set()
-	get set from scene
-    	IF checkName(set) & checkReferences(set)
-        		print "This set is ok"
-    	END IF
-
-	centerObject(set)
-	roundAttributes(set)
-	deleteBlankNodes()
-END FUNCTION
-
-
-Check camera aperture
-
-FUNCTION checkAperture(camera)
-	IF camera aperture is 16:9
-		RETURN true
-	ELSE
-		print “Camera aperture is invalid”
-		RETURN false
-	END IF
-END FUNCTION
-
-
-
-Check camera focal length
-
-FUNCTION checkFocalLength(camera)
-	focalLengths = {...}
-	IF camera focalLength in focalLenghts
-		RETURN true
-	ELSE
-print “Camera focal length is invalid”
-		RETURN false
-	END IF
-END FUNCTION
-
-
-
-Check camera f-stop
-
-FUNCTION checkFStop(camera)
-	fStops = {...}
-	IF camera fStop in fStops
-		RETURN true
-	ELSE
-		print “Camera f-stop is invalid”
-		RETURN false
-	END IF
-END FUNCTION
-
-Check camera with the functions above
-
-FUNCTION camera()
-	get camera from scene
-	IF checkAperture(camera) & checkFocalLength(camera) & checkFStop(camera)
-		print “This camera is ok”
-	END IF
-END FUNCTION
+cmds.showWindow(window)
