cf7f9cb4cd22e64b9664cae147d60c5aba3cdffe
Darren Bui <thanhdat.d.bui-1@student.uts.edu.au>
2023-11-02 17:17:07 +1100

    updated ui for selection reference tool

--- Scripts/Light Scene Builder/selection reference tool.py
+++ Scripts/Light Scene Builder/selection reference tool.py
@@ -13,27 +13,36 @@ def deselectAll():
     
 def referenceSelected():
     for obj in sel:
-        cmds.setAttr(obj + '.v', 1)
-        cmds.select(obj)
+        cmds.setAttr(mb + '.v', 1)
+        cmds.select(mb)
         cmds.file(path + '_' + '.mb', 
             es = True,
-            type = 'MBexport')
+            type = 'mayaBinary')
         cmds.setAttr(mb + '.v', 0)
     
     
 def showUI():
     
-    myWindow = cmds.window(title = "Selection and Referencing Tool", widthHeight =(200,200))
+    myWindow = cmds.window(title = "Selection and Referencing Tool", widthHeight =(250,200))
     cmds.columnLayout()
-  
+    
+    
+    cmds.separator(h=10)      
+    cmds.text('Click to select all selectable asssets')
+    cmds.separator(h=5) 
     cmds.button(label = "Select all assets", command = "selectAll()")
-    cmds.separator(h=10)
+    cmds.separator(h=20)
     
+    cmds.text('Click to deselect all currently selected asssets')
+    cmds.separator(h=5) 
     cmds.button(label = "Deselect all assets", command = "deselectAll()")
-    cmds.separator(h=10)
+    cmds.separator(h=20)
     
+    cmds.text('Click to reference all current selected assets')
+    cmds.separator(h=5) 
     cmds.button(label = "Convert selected assets to references", command = "referenceSelected()")
-    cmds.separator(h=10)
+    cmds.separator(h=20)
+    
     cmds.showWindow(myWindow)
     
 showUI()
