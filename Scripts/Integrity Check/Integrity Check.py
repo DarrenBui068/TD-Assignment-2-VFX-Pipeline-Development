d932e35d41c0557a5b8a1b7ce5ff16f8b4f7db97
dadysherwin1 <86547852+dadysherwin1@users.noreply.github.com>
2023-11-02 14:06:28 +1100

    Linked checkboxes
    - Ticking General will select every checkbox under General
    - Unticking General will unselect every checkbox under General
    - Unticking a child in General will untick General

--- Scripts/Integrity Check/Integrity Check.py
+++ Scripts/Integrity Check/Integrity Check.py
@@ -3,46 +3,92 @@ import maya.cmds as cmds
 if cmds.window('IntegrityCheck', exists = True):
     cmds.deleteUI('IntegrityCheck')
 window1 = cmds.window('IntegrityCheck', resizeToFitChildren=True)
-cmds.columnLayout(columnAttach=("left", 10))
+#cmds.columnLayout(columnAttach=("left", 10))
+cmds.columnLayout(adjustableColumn=True, columnOffset=("both", 10))
 cmds.separator(h=10,style='none')
 
 cmds.text("Select properties to check")
 cmds.separator(h=10,style='none')
 
 # General
-cmds.checkBox("General", annotation="Selects/deselects all General checks")
+def generalOn(*args):
+    cmds.checkBox("Unused_nodes", edit=True, value=True)
+    cmds.checkBox("Asset_naming", edit=True, value=True)
+    cmds.checkBox("Node_hierarchy", edit=True, value=True)
+    cmds.checkBox("Reference_errors", edit=True, value=True)
+    cmds.checkBox("Very_small_decimals", edit=True, value=True)
+def generalOff(*args):
+    cmds.checkBox("Unused_nodes", edit=True, value=False)
+    cmds.checkBox("Asset_naming", edit=True, value=False)
+    cmds.checkBox("Node_hierarchy", edit=True, value=False)
+    cmds.checkBox("Reference_errors", edit=True, value=False)
+    cmds.checkBox("Very_small_decimals", edit=True, value=False)
+def generalChildOff(*args):
+    cmds.checkBox("General", edit=True, value=False)
+cmds.checkBox("General", annotation="Selects/deselects all General checks", onCommand=generalOn, offCommand=generalOff)
 cmds.columnLayout(columnAttach=("left", 35))
-cmds.checkBox("Unused nodes", annotation="Check if theres any empty objects")
-cmds.checkBox("Asset naming", annotation="Checks object names with naming convention")
-cmds.checkBox("Node hierarchy", annotation="Checks if the layout of the scene is readable for publishing")
-cmds.checkBox("Reference errors", annotation="Checks for invalid references")
-cmds.checkBox("Very small decimals", annotation="Checks for abnormally long (5+) decimal points")
+cmds.checkBox("Unused nodes", annotation="Check if theres any empty objects", offCommand=generalChildOff)
+cmds.checkBox("Asset naming", annotation="Checks object names with naming convention", offCommand=generalChildOff)
+cmds.checkBox("Node hierarchy", annotation="Checks if the layout of the scene is readable for publishing", offCommand=generalChildOff)
+cmds.checkBox("Reference errors", annotation="Checks for invalid references", offCommand=generalChildOff)
+cmds.checkBox("Very small decimals", annotation="Checks for abnormally long (5+) decimal points", offCommand=generalChildOff)
 cmds.separator(h=10,style='none')
 
 # Layout
+def layoutOn(*args):
+    cmds.checkBox("Camera_apeture", edit=True, value=True)
+    cmds.checkBox("Focal_length", edit=True, value=True)
+    cmds.checkBox("F_Stop", edit=True, value=True)
+def layoutOff(*args):
+    cmds.checkBox("Camera_apeture", edit=True, value=False)
+    cmds.checkBox("Focal_length", edit=True, value=False)
+    cmds.checkBox("F_Stop", edit=True, value=False)
+def layoutChildOff(*args):
+    cmds.checkBox("Layout", edit=True, value=False)
 cmds.setParent("..")
-cmds.checkBox("Layout", annotation="Selects/deselects all Layout checks")
+cmds.checkBox("Layout", annotation="Selects/deselects all Layout checks", onCommand=layoutOn, offCommand=layoutOff)
 cmds.columnLayout(columnAttach=("left", 35))
-cmds.checkBox("Camera apeture", annotation="Checks if the camera apeture is 16:9 aspect ratio")
-cmds.checkBox("Focal length", annotation="Checks if the camera focal length is an appropriate value (12, 14, 16, 18, 21, 25, 27, 32, 35, 40, 50, 65, 75, 100, 135, 150)")
-cmds.checkBox("F-Stop", annotation="Checks if the camera f-stop is an appropriate value (1.3, 2, 2.8, 4, 5.6, 8, 11, 16, 22)")
+cmds.checkBox("Camera apeture", annotation="Checks if the camera apeture is 16:9 aspect ratio", offCommand=layoutChildOff)
+cmds.checkBox("Focal length", annotation="Checks if the camera focal length is an appropriate value (12, 14, 16, 18, 21, 25, 27, 32, 35, 40, 50, 65, 75, 100, 135, 150)", offCommand=layoutChildOff)
+cmds.checkBox("F-Stop", annotation="Checks if the camera f-stop is an appropriate value (1.3, 2, 2.8, 4, 5.6, 8, 11, 16, 22)", offCommand=layoutChildOff)
 cmds.separator(h=10,style='none')
 
 # Set pieces
+def setPieceOn(*args):
+    cmds.checkBox("Transform_Set_Piece", edit=True, value=True)
+    cmds.checkBox("Pivot_Set_Piece", edit=True, value=True)
+def setPieceOff(*args):
+    cmds.checkBox("Transform_Set_Piece", edit=True, value=False)
+    cmds.checkBox("Pivot_Set_Piece", edit=True, value=False)
+def setPieceChildOff(*args):
+    cmds.checkBox("Set_Piece", edit=True, value=False)
 cmds.setParent("..")
-cmds.checkBox("Set Piece", annotation="Selects/deselects all Set Piece checks")
+cmds.checkBox("Set Piece", annotation="Selects/deselects all Set Piece checks", onCommand=setPieceOn, offCommand=setPieceOff)
 cmds.columnLayout(columnAttach=("left", 35))
-cmds.checkBox("Transform", annotation="Checks if the transform is at the origin")
-cmds.checkBox("Pivot", annotation="Checks if the pivot is at the origin")
+cmds.checkBox("Transform Set Piece", label="Transform", annotation="Checks if the transform is at the origin", offCommand=setPieceChildOff)
+cmds.checkBox("Pivot Set Piece", label="Pivot", annotation="Checks if the pivot is at the origin", offCommand=setPieceChildOff)
 cmds.separator(h=10,style='none')
 
 # Sets
+def setOn(*args):
+    cmds.checkBox("Transform_Set", edit=True, value=True)
+    cmds.checkBox("Pivot_Set", edit=True, value=True)
+    cmds.checkBox("Asset_version", edit=True, value=True)
+def setOff(*args):
+    cmds.checkBox("Transform_Set", edit=True, value=False)
+    cmds.checkBox("Pivot_Set", edit=True, value=False)
+    cmds.checkBox("Asset_version", edit=True, value=False)
+def setChildOff(*args):
+    cmds.checkBox("Set", edit=True, value=False)
 cmds.setParent("..")
-cmds.checkBox("Set", annotation="Selects/deselects all Set checks")
+cmds.checkBox("Set", annotation="Selects/deselects all Set checks", onCommand=setOn, offCommand=setOff)
 cmds.columnLayout(columnAttach=("left", 35))
-cmds.checkBox("Transform", annotation="Checks if the transform is at the origin")
-cmds.checkBox("Pivot", annotation="Checks if the pivot is at the origin")
-cmds.checkBox("Asset version", annotation="Checks if each referenced asset is the latest version")
+cmds.checkBox("Transform Set", label="Transform", annotation="Checks if the transform is at the origin", offCommand=setChildOff)
+cmds.checkBox("Pivot Set", label="Pivot", annotation="Checks if the pivot is at the origin", offCommand=setChildOff)
+cmds.checkBox("Asset version", annotation="Checks if each referenced asset is the latest version", offCommand=setChildOff)
 cmds.separator(h=10,style='none')
+    
+cmds.setParent("..")
+cmds.button("Continue")
 
-cmds.showWindow(window)
+cmds.showWindow(window1)
