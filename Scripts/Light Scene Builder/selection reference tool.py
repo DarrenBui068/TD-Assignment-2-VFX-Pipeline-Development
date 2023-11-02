4e0ac0733d1e1aa35a91511ae89cd1b76fc76099
DarrenBui068 <thanhdat.d.bui-1@student.uts.edu.au>
2023-11-03 10:39:32 +1100

    lighting scene builder script added to main branch

---
+++ Scripts/Light Scene Builder/selection reference tool.py
@@ -0,0 +1,60 @@
+import maya.cmds as cmds
+
+sel = cmds.ls(sl=True)
+
+path = cmds.workspace (rd = True , q = True)
+print(path)
+
+def selectAll():
+    cmds.select(allDagObjects=True) 
+    
+def deselectAll():
+    cmds.select(clear = True)
+    
+def referenceSelected():
+    for obj in cmds.ls(assemblies=True):
+        print(obj)
+        cmds.setAttr(obj + '.v', 1)
+        cmds.select(obj)
+        cmds.file(path + obj + '.mb', 
+            es = True,
+            type = 'mayaBinary')
+        cmds.setAttr(obj + '.v', 0)
+    
+    
+def showUI():
+    
+    myWindow = cmds.window(title = "Selection and Referencing Tool", widthHeight =(250,400))
+    cmds.columnLayout()
+    
+    
+    cmds.separator(h=10)      
+    cmds.text('Click to select all selectable asssets')
+    cmds.separator(h=5) 
+    cmds.button(label = "Select all assets", command = "selectAll()")
+    cmds.separator(h=20)
+    
+    cmds.text('Click to deselect all currently selected asssets')
+    cmds.separator(h=5) 
+    cmds.button(label = "Deselect all assets", command = "deselectAll()")
+    cmds.separator(h=20)
+    
+    cmds.text('Click to reference all current selected assets')
+    cmds.separator(h=5) 
+    cmds.button(label = "Convert selected assets to references", command = "referenceSelected()")
+    cmds.separator(h=20)
+    
+    cmds.text('Use to check selected asset version')
+    cmds.separator(h=5) 
+    cmds.button(label = "Check selected version", command = "checkVersion()")
+    cmds.separator(h=20)
+    
+    cmds.text('Use to update out of date asset')
+    cmds.separator(h=5) 
+    cmds.button(label = "Update Asset version", command = "updateVersion()")
+    cmds.separator(h=20)    
+    
+    cmds.showWindow(myWindow)
+    
+showUI()
+
